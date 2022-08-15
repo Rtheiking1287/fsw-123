71fa493a298dcbfae485190e73365800faab2a7a
Rtheiking1287 <100746144+Rtheiking1287@users.noreply.github.com>
2022-08-14 17:07:17 -0700

    Add files via upload

---
+++ week1/TodoList.js
@@ -0,0 +1,16 @@
+function TodoList() {
+    return (
+        <>
+        <h1>List of Todos</h1>
+        <ul>
+            <li><input type='checkbox'></input>Clean the living room</li>
+            <li><input type='checkbox'></input>Prep trauma bag</li>
+            <li><input type='checkbox'></input>Do workout</li>
+            <li><input type='checkbox'></input>Create training plan for FY23</li>
+        </ul>
+        </>
+
+    );
+}
+
+export default TodoList;
