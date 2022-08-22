990f0b86a4dbbf2ec5eaf4417b5b591add5327d8
Rtheiking1287 <100746144+Rtheiking1287@users.noreply.github.com>
2022-08-22 00:06:50 -0700

    Add files via upload

---
+++ week2/boxes/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <h1></h1>
+  );
+}
+
+export default App;
+
---
+++ week2/boxes/Card.js
@@ -0,0 +1,12 @@
+
+
+function Card() {
+
+    return (
+        <h1>Title 1</h1>,
+        <h2>Sub Title 1</h2>,
+        <h3>Hello world 1!</h3>
+    );
+}
+
+export default Card;
